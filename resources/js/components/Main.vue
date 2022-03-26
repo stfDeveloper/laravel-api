03d24fa22be9bbb5acf431f9bd3181572bba19b8
stfDeveloper <sstefancosmin@icloud.com>
2022-03-26 18:36:25 +0100

    finito, da fare css

---
+++ resources/js/components/Main.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <ul>
+          <li v-for="(post ,index) in posts" :key="index">
+              {{post.title}}
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Main',
+    data(){
+        return{
+            posts:[]
+        };
+    },
+    created(){
+        axios.get('/api/posts')
+    .then((response)=> {
+        // handle success
+        this.posts = response.data;
+    })
+
+    }
+}
+</script>
+
+<style>
+
+</style>
